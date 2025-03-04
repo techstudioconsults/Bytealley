0e70501ee919319c5d947c61ebc54e19d35a3ec9
kingsley <kinxly@gmail.com>
2025-03-04 09:59:18 +0100

    fix: removed all sensitive data into env

--- src/features/funnel/config/index.ts
+++ src/features/funnel/config/index.ts
@@ -1,5 +1,5 @@
 export const editorOptions = {
-  licenseKey: "YOUR_LICENSE_KEY",
+  licenseKey: process.env.NEXT_PUBLIC_GRAPESJS_LICENCE_KEY as string,
   theme: "light" as "light" | "dark" | "auto",
   project: {
     default: {
@@ -7,66 +7,10 @@ export const editorOptions = {
         {
           name: "Home",
           component: `<h1 style="padding: 2rem; text-align: center">
-                    Hello Studio 👋
+                   Welcome to Byte-alley 👋
                   </h1>`,
         },
       ],
     },
   },
-  // layout: {
-  //   default: {
-  //     type: "row",
-  //     style: { height: "100%" },
-  //     children: [
-  //       { type: "sidebarLeft" },
-  //       {
-  //         type: "canvasSidebarTop",
-  //         sidebarTop: {
-  //           leftContainer: {
-  //             buttons: ({ items }: { items: any[] }) => [
-  //               ...items,
-  //               {
-  //                 id: "publish-funnel",
-  //                 label: "Save and Continue",
-  //                 style: { backgroundColor: "green", color: "#FFFFFF" },
-  //                 onClick: ({ editor }: any) => {
-  //                   // Use the editor's Modal API to show a modal
-  //                   editor.Modal.open({
-  //                     title: "Save and Continue", // Modal title
-  //                     content: `
-  //                       <div>
-  //                         <p>Are you sure you want to save and continue?</p>
-  //                       </div>
-  //                     `, // Modal content (HTML)
-  //                     buttons: [
-  //                       {
-  //                         id: "confirm-save",
-  //                         label: "Confirm",
-  //                         className: "btn-primary",
-  //                         onClick: () => {
-  //                           console.log("Save confirmed!");
-  //                           editor.Modal.close(); // Close the modal
-  //                         },
-  //                       },
-  //                       {
-  //                         id: "cancel-save",
-  //                         label: "Cancel",
-  //                         className: "btn-secondary",
-  //                         onClick: () => {
-  //                           console.log("Save cancelled!");
-  //                           editor.Modal.close(); // Close the modal
-  //                         },
-  //                       },
-  //                     ],
-  //                   });
-  //                 },
-  //               },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //       { type: "sidebarRight" },
-  //     ],
-  //   },
-  // },
 };
