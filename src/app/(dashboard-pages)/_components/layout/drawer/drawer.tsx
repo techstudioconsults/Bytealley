af2ff032ddcf48801e360f6023a1ae03eb2fb601
Kingsley Ifijeh <kinxly@gmail.com>
2025-04-06 19:21:20 +0100

    fix: dashboard layout has better responsiveness

--- src/app/(dashboard-pages)/_components/layout/drawer/drawer.tsx
+++ src/app/(dashboard-pages)/_components/layout/drawer/drawer.tsx
@@ -1,32 +1,34 @@
-"use client";
+// "use client";
 
-import { MenuIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
-import { FC, useEffect, useState } from "react";
+// import { MenuIcon } from "lucide-react";
+// import { usePathname } from "next/navigation";
+// import { FC, useEffect, useState } from "react";
 
-import { DialogTitle } from "~/components/ui/dialog";
-import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
-import { cn } from "~/utils/utils";
-import { Sidebar } from "../sidebar/sidebar";
+// import { DialogTitle } from "~/components/ui/dialog";
+// import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
+// import { cn } from "~/utils/utils";
 
-export const Drawer: FC<ISidebarProperties> = () => {
-  const pathName = usePathname();
-  const [isOpen, setIsOpen] = useState(false);
-  const [isEditor, setHasEditor] = useState(false);
+// // import { Sidebar } from "../sidebar/sidebar";
 
-  useEffect(() => {
-    setHasEditor(pathName.includes(`editor`));
-  }, [pathName]);
+// export const Drawer: FC<ISidebarProperties> = () => {
+//   const pathName = usePathname();
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [isEditor, setHasEditor] = useState(false);
 
-  return (
-    <Sheet open={isOpen} onOpenChange={setIsOpen}>
-      <SheetTrigger className={cn(`xl:hidden`, isEditor && `xl:block`)}>
-        <MenuIcon />
-      </SheetTrigger>
-      <SheetContent side={`left`}>
-        <DialogTitle hidden>Navigation Menu</DialogTitle>
-        <Sidebar onClose={() => setIsOpen(false)} />
-      </SheetContent>
-    </Sheet>
-  );
-};
+//   useEffect(() => {
+//     setHasEditor(pathName.includes(`editor`));
+//   }, [pathName]);
+
+//   return (
+//     <Sheet open={isOpen} onOpenChange={setIsOpen}>
+//       <SheetTrigger className={cn(`xl:hidden`, isEditor && `xl:block`)}>
+//         <MenuIcon />
+//       </SheetTrigger>
+//       <SheetContent side={`left`}>
+//         <DialogTitle hidden>Navigation Menu</DialogTitle>
+//         {/* <Sidebar onClose={() => setIsOpen(false)} /> */}
+//       </SheetContent>
+//     </Sheet>
+//   );
+// };
+export {};
